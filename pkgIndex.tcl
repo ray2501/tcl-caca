885e61807673c5d41c0922eb8350f82d59290df5
Danilo Chang <ray2501@gmail.com>
2023-12-14 21:32:44 +0800

    Bump version to v0.8

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -2,9 +2,9 @@
 # Tcl package index file, version 1.1
 #
 if {[package vsatisfies [package provide Tcl] 9.0-]} {
-    package ifneeded caca 0.7 \
-	    [list load [file join $dir libtcl9caca0.7.so] [string totitle caca]]
+    package ifneeded caca 0.8 \
+	    [list load [file join $dir libtcl9caca0.8.so] [string totitle caca]]
 } else {
-    package ifneeded caca 0.7 \
-	    [list load [file join $dir libcaca0.7.so] [string totitle caca]]
+    package ifneeded caca 0.8 \
+	    [list load [file join $dir libcaca0.8.so] [string totitle caca]]
 }
--- tcl-caca.spec
+++ tcl-caca.spec
@@ -6,7 +6,7 @@
 %define packagename caca
 
 Name:           tcl-caca
-Version:        0.7
+Version:        0.8
 Release:        0
 Summary:        Tcl bindings for Colour ASCII Art library (libcaca) 
 License:        MIT
